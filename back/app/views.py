1428ba5efd2565e41dba0983dd2901c04d0e68f6
Asiberus <raphael.beekmann@gmail.com>
2022-09-12 23:00:56 +0200

    feat(back): add welcome view

--- back/app/views.py
+++ back/app/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class WelcomeView(TemplateView):
+    template_name = 'welcome.html'
