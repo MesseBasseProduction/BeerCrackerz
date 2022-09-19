e7bca291d27a102cdd33b2db11f4ef549049fc12
Asiberus <raphael.beekmann@gmail.com>
2022-09-19 19:25:06 +0200

    feat: set user login for spot, shop, bar creation

--- back/api/serializers/spot/spot.py
+++ back/api/serializers/spot/spot.py
@@ -1,21 +1,14 @@
 from rest_framework import serializers
 
-from app.models import User
 from app.models.spot import Spot
-from app.utils import get_or_raise_error
 
 
 class SpotSerializer(serializers.ModelSerializer):
-    userId = serializers.ModelField(model_field=Spot()._meta.get_field('user'), required=True, allow_null=False)
-
     class Meta:
         model = Spot
-        fields = ('id', 'name', 'description', 'lng', 'lat', 'userId')
+        fields = ('id', 'name', 'description', 'lng', 'lat')
 
-    def validate(self, data):
-        # Map userId to user object
-        if 'userId' in data:
-            data['user'] = get_or_raise_error(User, id=data.pop('userId'),
-                                              error=serializers.ValidationError('This user doesn\'t exist'))
-
-        return data
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+        return super().create(validated_data)
