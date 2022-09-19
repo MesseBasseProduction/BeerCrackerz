309c092fbe206c61db5f08c2d8be62a4c7d43d9c
Asiberus <raphael.beekmann@gmail.com>
2022-09-19 19:50:25 +0200

    feat(back): add endpoint to get user connected

--- back/api/views/user.py
+++ back/api/views/user.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.response import Response
 
 from api.serializers.user import UserSerializer
 from app.models import User
@@ -6,4 +9,12 @@ from app.models import User
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = UserSerializer
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def user_connected(request):
+    user = UserSerializer(request.user)
+    return Response(user.data)
