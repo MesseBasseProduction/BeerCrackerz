b470d707a46f59df05d3283c47554103387dd8f3
Raphael Beekmann <raphael.beekmann@gmail.com>
2022-10-24 18:42:15 +0200

    feat(back): remove file if user's profile picture is updated or deleted

--- back/app/models/user.py
+++ back/app/models/user.py
@@ -1,10 +1,34 @@
+import os
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_picture/', null=True)
+
+
+@receiver(models.signals.pre_save, sender=User)
+def pre_save_profile_picture(sender, instance, **kwargs):
+    try:
+        old_profile_picture = sender.objects.get(id=instance.id).profile_picture
+    except sender.DoesNotExist:
+        old_profile_picture = None
+    new_profile_picture = instance.profile_picture
+    if not old_profile_picture or not new_profile_picture:
+        return
+
+    if old_profile_picture.path != new_profile_picture.path:
+        if os.path.isfile(old_profile_picture.path):
+            os.remove(old_profile_picture.path)
+
+
+@receiver(models.signals.post_delete, sender=User)
+def post_delete_profile_picture(sender, instance, **kwargs):
+    if instance.profile_picture:
+        instance.profile_picture.delete(save=False)
 
 
 def get_default_user():
