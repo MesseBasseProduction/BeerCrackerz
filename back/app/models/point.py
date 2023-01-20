1548406625acc04d81ead226906bd84b7b3b4d5e
Raphael Beekmann <raphael.beekmann@gmail.com>
2023-01-20 19:15:20 +0100

    chore(back): change description maxlength to 500.
    Merge migrations

--- back/app/models/point.py
+++ back/app/models/point.py
@@ -3,7 +3,7 @@
 
 class Point(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=280, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
     lng = models.FloatField()
     lat = models.FloatField()
     creation_date = models.DateField(auto_now_add=True)
