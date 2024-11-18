a4a6863766aa0999aa24e98773510ccfab338e47
Raphael Beekmann <raphael.beekmann@gmail.com>
2024-11-18 21:24:31 +0100

    feat: add restaurant type

--- front/src/js/utils/enums/MarkInfosEnum.js
+++ front/src/js/utils/enums/MarkInfosEnum.js
@@ -1,14 +1,14 @@
 export default Object.freeze({
-  spot: {
-    types: ['forest', 'river', 'cliff', 'mountain', 'beach', 'sea', 'city', 'pov', 'lake'],
-    modifiers: ['bench', 'covered', 'toilet', 'store', 'trash', 'parking']
-  },
-  shop: {
-    types: ['store', 'super', 'hyper', 'cellar'],
-    modifiers: ['bio', 'craft', 'fresh', 'card', 'choice']
-  },
-  bar: {
-    types: ['regular', 'snack', 'cellar', 'rooftop'],
-    modifiers: ['tobacco', 'food', 'card', 'choice', 'outdoor']
-  }
+    spot: {
+        types: ['forest', 'river', 'cliff', 'mountain', 'beach', 'sea', 'city', 'pov', 'lake'],
+        modifiers: ['bench', 'covered', 'toilet', 'store', 'trash', 'parking']
+    },
+    shop: {
+        types: ['store', 'super', 'hyper', 'cellar'],
+        modifiers: ['bio', 'craft', 'fresh', 'card', 'choice']
+    },
+    bar: {
+        types: ['regular', 'snack', 'cellar', 'rooftop', 'restaurant'],
+        modifiers: ['tobacco', 'food', 'card', 'choice', 'outdoor']
+    }
 });
